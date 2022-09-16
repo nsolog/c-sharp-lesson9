8cce44e733056d583bbb8292e6e8ac174d4ef570
Nikita Smetannikov <smetannikovnikita@yandex.ru>
2022-09-16 15:20:25 +0300

    Task resolved

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (M < N)
+{
+    int multiple = 1;
+    while (M <= N)
+    {
+        if (M > 0) multiple *= M;
+        M++;
+    }
+    Console.Write($"Произведение всех натуральных чисел в прромежутке M:N = {multiple}");
+}
+else
+{
+    Console.WriteLine("В указанном промежутке нет натуральных чисел");
+}
